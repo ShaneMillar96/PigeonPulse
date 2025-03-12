f09bd428515d1e2c5389a198d5fcf4df7b0924bb
ShaneMillar <151863557+ShaneMillar@users.noreply.github.com>
2025-03-12 20:16:07 +0000

    deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,16 @@ cd /home/ec2-user/PigeonPulse/server/src/PigeonPulse.Api
 export ASPNETCORE_ENVIRONMENT=Development
 nohup dotnet run --urls "http://0.0.0.0:5264" > backend.log 2>&1 &
 
-echo "Starting Frontend (React)..."
+echo "Preparing Frontend (React)..."
 cd /home/ec2-user/PigeonPulse/client
+echo "Cleaning old dependencies..."
 rm -rf node_modules package-lock.json
-nohup npm install && npm run dev -- --host > frontend.log 2>&1 &
+
+echo "Installing dependencies..."
+npm install
+
+echo "Building frontend..."
+npm run build
+
+echo "Starting frontend..."
+nohup npm run dev -- --host > frontend.log 2>&1 &
