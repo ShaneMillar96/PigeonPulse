af84e683e9d756208ca14a72b6bea17bd1ddd10f
ShaneMillar <151863557+ShaneMillar@users.noreply.github.com>
2025-03-12 18:50:07 +0000

    configs

--- server/src/PigeonPulse.Dal/EnvironmentVariables.cs
+++ server/src/PigeonPulse.Dal/EnvironmentVariables.cs
@@ -6,23 +6,7 @@ public static class EnvironmentVariables
 {
     private static string DbConnectionStringKey => "DbConnectionString";
 
-    public static string? DbConnectionString
-    {
-        get
-        {
-            // Check if running on AWS (EC2 instance)
-            bool isRunningOnEC2 = Environment.GetEnvironmentVariable("AWS_EXECUTION_ENV") != null;
+    //public static string? DbConnectionString => DbConnectionStringKey.GetValue("Server=localhost;Port=5432;Database=pigeonpulse-dev;User Id=pigeonpulse;Password=password1;");
+    public static string? DbConnectionString => DbConnectionStringKey.GetValue("Host=pigeonpulse-dev.cbiqqummoi67.eu-north-1.rds.amazonaws.com;Port=5432;Database=pigeonpulse-dev;Username=pigeonpulse;Password=pigeonpulse96;");
 
-            if (isRunningOnEC2)
-            {
-                // Use Amazon RDS connection
-                return DbConnectionStringKey.GetValue("Host=pigeonpulse-dev.cbiqqummoi67.eu-north-1.rds.amazonaws.com;Port=5432;Database=pigeonpulse-dev;Username=pigeonpulse;Password=pigeonpulse96;");
-            }
-            else
-            {
-                // Use Localhost connection for development
-                return DbConnectionStringKey.GetValue("Server=localhost;Port=5432;Database=pigeonpulse-dev;User Id=pigeonpulse;Password=password1;");
-            }
-        }
-    }
 }
