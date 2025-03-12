6886cbbf59c2fc9a35328b7cd101429f1a6f8b05
ShaneMillar <151863557+ShaneMillar@users.noreply.github.com>
2025-03-12 14:48:25 +0000

    before_install.sh update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e  # Stop script on first error
+
+# Remove curl-minimal if it exists to prevent conflicts
+sudo yum remove -y curl-minimal || true
+sudo yum install -y curl || true
+
+echo "Curl installed successfully."
+
 echo "Stopping existing application (if running)..."
 sudo pkill -f "dotnet"
 sudo pkill -f "node"
