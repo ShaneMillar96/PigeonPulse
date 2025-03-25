53744cc66f9d2c1af2841c07f7b5f3acc6e27e66
ShaneMillar <151863557+ShaneMillar@users.noreply.github.com>
2025-03-25 12:11:28 +0000

    Default sex to male for existing pigeons

--- database/application/migrations/V1.1__remove_strain_remove_name_add_sex.sql
+++ database/application/migrations/V1.1__remove_strain_remove_name_add_sex.sql
@@ -1,6 +1,8 @@
--- File: database/application/migrations/V1.1__remove_strain_remove_name_add_sex.sql
-
 ALTER TABLE pigeons
 DROP COLUMN strain,
 DROP COLUMN name,
-ADD COLUMN sex VARCHAR(10) NOT NULL;
+ADD COLUMN sex VARCHAR(10) NOT NULL DEFAULT 'Male';
+
+UPDATE pigeons
+SET sex = 'Male'
+WHERE sex IS NULL;
