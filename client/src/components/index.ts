0f82d009e2d1ec14e538f3a6aa7b87540c166938
ShaneMillar <151863557+ShaneMillar@users.noreply.github.com>
2025-03-27 17:05:59 +0000

    typo fix

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,5 @@
 export {  Footer } from './layouts/Footer';
-export {  Navbar } from './layouts/NavBar';
+export {  Navbar } from './layouts/Navbar';
 export {  Button } from './ui/Button';
 export {  Modal } from './ui/Modal';
 export { Input } from './forms/Input';
